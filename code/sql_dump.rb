76b56d59f3cf1fb1f20b8b5b8fcd4533198ca492
Dylan Northrup <github@doc-x.net>
2014-05-21 16:33:46 -0700

    Updated Hex.rb so I don't have to instantiate a Card object to call the dump_sql_table_format method

--- code/sql_dump.rb
+++ code/sql_dump.rb
@@ -8,7 +8,7 @@ foo = Hex::Collection.new
 #foo.load_set('Set001')
 foo.load_collection
 
-puts Hex::Card.new.dump_sql_table_format
+puts Hex::Card.dump_sql_table_format
 foo.cards.sort {|a, b| a.card_number.to_i <=> b.card_number.to_i}.each do |card|
   puts "#{card.to_sql}"
 #  puts "select name from cards where uuid = '#{card.uuid}';"
