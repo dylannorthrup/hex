032a6b3c47d6da66b234006baed0f4e56fa076c4
Dylan Northrup <github@doc-x.net>
2014-05-28 07:06:24 -0700

    Minor tweaks to get this back to intended behavior

--- code/collectable.rb
+++ code/collectable.rb
@@ -14,9 +14,9 @@ con = foo.get_db_con
 foo.load_collection(con)
 
 puts "SET NUMBER|CARD NUMBER|NAME|RARITY|COLOR|TYPE|SUB TYPE|FACTION|SOCKET COUNT|COST|ATK|HEALTH|TEXT|FLAVOR|RESTRICTION|ARTIST|ENTERS PLAY EXHAUSTED"
-foo.cards.sort {|a, b| a.card_number <=> b.card_number}.each do |card|
+foo.cards.sort {|a, b| a.card_number.to_i <=> b.card_number.to_i}.each do |card|
   # Skip non collectible cards (which all have card_number of 0)
-#  next if card.card_number == 0
+  next if card.card_number == "0"
 #  puts "Card: #{card}"
   puts "#{card.to_csv}"
 end
