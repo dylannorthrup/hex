c897ee077f7642f82a16ffb6a25808e6f3c93dc2
Dylan Northrup <github@doc-x.net>
2014-05-28 07:07:47 -0700

    Adding HTML'ified version of collectable

---
+++ code/collectable_html.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+#
+# Test out the Hex module
+
+# Print out HTTP headers
+puts "Content-type: text/html"
+puts ""
+
+require 'pry'
+$: << "/home/docxstudios/web/hex/code"
+require "Hex"
+require "mysql"
+#results = con.query("Select distinct set_id from cards")
+#results.each do |row|
+#  puts row
+#end
+
+foo = Hex::Collection.new
+con = foo.get_db_con
+foo.load_collection(con)
+
+puts "<head>"
+puts '<link rel="stylesheet" type="text/css" href="/hex/tables.css">'
+puts "</head>"
+
+puts '<div class="CSSTableGenerator" > '
+puts '<table>'
+puts Hex::Card.dump_html_table_header
+foo.cards.sort {|a, b| a.card_number.to_i <=> b.card_number.to_i}.each do |card|
+  next if card.card_number == "0"
+#  puts card.card_number, card.name
+  puts "#{card.to_html_table}"
+end
+
+puts '</table>'
+
+#binding.pry
