e5798bfd8082100c5a93bf90202842b989caff6c
Dylan Northrup <github@doc-x.net>
2014-07-19 16:28:04 -0700

    Initial

---
+++ code/legendary_rare_prices.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+#
+# get prices for rare and legendary cards for both gold and platinum from Hex price data
+
+$: << "/home/docxstudios/web/hex/code"
+require 'prices'
+
+####### MAIN SECTION
+foo = Hex::Collection.new
+con = foo.get_db_con
+lines = read_db(con, "and c.rarity regexp 'Legendary|Rare'")                      # Get data from database
+parse_lines(lines)                        # Compile that data into a useable form
+puts @card_names.size
+print_filtered_output(@card_names, '\[Legendary\]')
+print_filtered_output(@card_names, '\[Rare\]')
