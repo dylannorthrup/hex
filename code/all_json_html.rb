ad471b2b6150aa1336efc83f470339df594544e1
Dylan Northrup <github@doc-x.net>
2014-08-12 11:08:41 -0700

    Altering so it grabs info from the database

--- code/all_json_html.rb
+++ code/all_json_html.rb
@@ -9,7 +9,8 @@ puts ""
 $: << "/home/docxstudios/web/hex/code"
 require "Hex"
 foo = Hex::Collection.new
-foo.load_set('Set001')
+con = foo.get_db_con
+foo.load_set('001', con)
 
 puts "<head>"
 puts '<link rel="stylesheet" type="text/css" href="/hex/tables.css">'
