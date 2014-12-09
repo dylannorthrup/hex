7c2575e917268cd7822b72c1a2d76feec3898785
Dylan Northrup <github@doc-x.net>
2014-12-09 09:28:49 -0800

    Set this up to dump set 2 data

--- code/sql_dump.rb
+++ code/sql_dump.rb
@@ -3,9 +3,10 @@
 # Grab cards and dump them to SQL
 
 $: << "/home/docxstudios/web/hex/code"
-require "Hex"
+require "Hex_test"
 foo = Hex::Collection.new
-foo.load_collection
+#foo.load_collection
+foo.load_set(set_name='Set02_PVP')
 
 puts Hex::Card.dump_sql_header
 foo.cards.sort {|a, b| a.card_number.to_i <=> b.card_number.to_i}.each do |card|
