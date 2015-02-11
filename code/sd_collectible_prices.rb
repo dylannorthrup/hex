98fa588ee2c53bb74d4652921f82121e001afce6
Dylan Northrup <github@doc-x.net>
2015-02-11 05:54:50 -0800

    Initial

---
+++ code/sd_collectible_prices.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+#
+# get prices for rare and legendary cards for both gold and platinum from Hex price data
+
+$: << "/home/docxstudios/web/hex/code"
+require 'prices'
+
+####### MAIN SECTION
+foo = Hex::Collection.new
+con = foo.get_db_con
+lines = read_db(con, "and c.rarity regexp 'Legendary|Rare' and c.set_id regexp '002'")                      # Get data from database
+parse_lines(lines)                        # Compile that data into a useable form
+#puts "====== LEGENDARY CARDS ======"
+print_pdcsv_output(@card_names, '\[Legendary\]', ' AA\' \[')
+#puts ""
+#puts "====== RARE CARDS ======"
+print_pdcsv_output(@card_names, '\[Rare\]', ' AA\' \[')
