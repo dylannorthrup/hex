9e34a40a3d447e6447047ec288822653ea06f91f
Dylan Northrup <github@doc-x.net>
2015-04-23 05:59:52 -0700

    So long ago I forget why i made this change

--- code/sd_collectible_prices.rb
+++ code/sd_collectible_prices.rb
@@ -8,8 +8,9 @@ require 'prices'
 ####### MAIN SECTION
 foo = Hex::Collection.new
 con = foo.get_db_con
-lines = read_db(con, "and c.rarity regexp 'Legendary|Rare' and c.set_id regexp '002'")                      # Get data from database
-parse_lines(lines)                        # Compile that data into a useable form
+filter = "and c.rarity regexp 'Legendary|Rare' and c.set_id regexp '^002$'"
+lines = read_db(con, filter)   # Get data from database
+parse_lines(lines)             # Compile that data into a useable form
 #puts "====== LEGENDARY CARDS ======"
 print_pdcsv_output(@card_names, '\[Legendary\]', ' AA\' \[')
 #puts ""
