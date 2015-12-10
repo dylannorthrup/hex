0d13c757e7ddac2f91dd4d2ab763edc94498823a
Dylan Northrup <github@doc-x.net>
2015-12-10 11:09:21 -0800

    A script to run the various price list scripts

---
+++ code/generate_dependant_price_lists.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# A wrapper script to run all of the "cron_price_gen.sh" scripts that aren't the 'all_prices_csv' script
+
+# This will be the list once we've converted some of the other scripts
+#for script in all_prices_with_uuids legendary_rare_detailed_price_info legendary_rare_csv legendary_rare_prices common_prices all_prices have-want; do 
+for script in all_prices_with_uuids legendary_rare_csv legendary_rare_prices common_prices all_prices have-want; do 
+  echo $script
+  time /home/docxstudios/cron_price_gen.sh $script
+done
