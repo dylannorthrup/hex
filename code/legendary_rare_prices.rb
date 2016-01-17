230df730ee003434ee3d9e3d6d34a8081f162794
Dylan Northrup <github@doc-x.net>
2016-01-17 10:25:29 -0800

    Making this work on cached data instead of doing DB lookups each run

--- code/legendary_rare_prices.rb
+++ code/legendary_rare_prices.rb
@@ -3,15 +3,22 @@
 # get prices for rare and legendary cards for both gold and platinum from Hex price data
 
 $: << "/home/docxstudios/web/hex/code"
-require 'prices'
+require 'Hex'
 
 ####### MAIN SECTION
+puts "Content-type: text\plain\n"
+
 foo = Hex::Collection.new
-con = foo.get_db_con
-lines = read_db(con, "and c.rarity regexp 'Legendary|Rare'")                      # Get data from database
-parse_lines(lines)                        # Compile that data into a useable form
+
+prices = foo.get_local_price_info
+lines = foo.get_card_list_from_db('rarity = "Legendary" AND type NOT LIKE "Equipment"')
 puts "====== LEGENDARY CARDS ======"
-print_filtered_output(@card_names, '\[Legendary\]')
-puts ""
+puts "Name ... Avg_price Currency [# of auctions] ... Avg_price Currency [# of auctions]"
+foo.print_local_info_for_cardlist(lines, prices)
+
+lines = foo.get_card_list_from_db('rarity = "Legendary" AND type NOT LIKE "Equipment"')
 puts "====== RARE CARDS ======"
-print_filtered_output(@card_names, '\[Rare\]')
+puts "Name ... Avg_price Currency [# of auctions] ... Avg_price Currency [# of auctions]"
+foo.print_local_info_for_cardlist(lines, prices)
+exit
+
