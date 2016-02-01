adcb064a1053783bcf2c654b8eb28556db697d5f
Dylan Northrup <github@doc-x.net>
2016-01-31 19:11:45 -0800

    Initial

---
+++ code/coe_collectible_prices.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+#
+# get prices for rare and legendary cards for both gold and platinum from Hex price data
+
+$: << "/home/docxstudios/web/hex/code"
+require 'Hex'
+
+foo = Hex::Collection.new
+foo.get_local_price_info
+foo.print_local_price_info_for_set("COE001")
