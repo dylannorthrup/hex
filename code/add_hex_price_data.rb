f5d9b1768b7aa6dda73e93ce622a25ca4488b84b
Dylan Northrup <github@doc-x.net>
2016-07-10 09:22:43 -0700

    Adding in bits to handle new UUID field

--- code/add_hex_price_data.rb
+++ code/add_hex_price_data.rb
@@ -35,15 +35,16 @@ def insert_data(sql_con=nil, lines=nil)
     parsed_line = line.gsub(/\r\n?/, "\n")
     # Run regexp against line and grab out interesting bits
     #if parsed_line.match(/^(.*),(GOLD|PLATINUM),(\d+),(.*),(\d+)$/)
-    if parsed_line.match(/^\s*(.*?)\s*,\s*(\d+)\s*,\s*(GOLD|PLATINUM)\s*,\s*(\d+)\s*,\s*(.*?)\s*$/)
+    if parsed_line.match(/^\s*(.*?)\s*,\s*(\d+)\s*,\s*(GOLD|PLATINUM)\s*,\s*(\d+)\s*,\s*(.*?)\s*,\s*([a-f0-9-]*?)\s*$/)
       name = $1
       rarity = $2
       currency = $3
       price = $4
       date = $5
+      uuid = $6
       # Do replacements afterward so we don't mess up match variables
       name.gsub!(/"/, '')   # Get rid of any double quotes
-      query = "INSERT INTO ah_data values ('#{Mysql.escape_string name}','#{Mysql.escape_string currency}',#{Mysql.escape_string price},'#{Mysql.escape_string date}',#{Mysql.escape_string rarity});"
+      query = "INSERT INTO ah_data values ('#{Mysql.escape_string name}','#{Mysql.escape_string currency}',#{Mysql.escape_string price},'#{Mysql.escape_string date}',#{Mysql.escape_string rarity},'#{Mysql.escape_string uuid}');"
       sql_con.query(query)
     else
       puts "ERROR: Line did not match regexp: '#{parsed_line}'"
