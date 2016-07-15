c479bda021448c3e73a986d199d7c3b2affd04d8
Dylan Northrup <github@doc-x.net>
2016-07-14 19:18:30 -0700

    Updated this to handle traits with spaces in them for certain traits a bit better

--- code/sample-searches.rb
+++ code/sample-searches.rb
@@ -4,7 +4,7 @@
 
 require 'cgi'
 
-@search_url = 'http://doc-x.net/hex/search.rb?'
+@search_url = 'http://doc-x.net/hex/search.rb?output_format=html_card_table&'
 @num_cols = 5
 
 # Helper for printing out search links
@@ -32,10 +32,20 @@ foo.load_collection(con)
 types = Hash.new(0)
 sub_types = Hash.new(0)
 
-traits = %w< Set_ID Type Cost Rarity Color Sub_Type Restriction Socket_Count Faction ATK Health Threshold_Color Threshold_Number >
-#traits = %w< ATK Health >
+# Make a big array of traits we want to look at. We order it this way so the output formatting
+# will work nicely
+traits = %w< Set_ID Type Cost Rarity Threshold Sub_Type Restriction Socket_Count Faction ATK Health >
 trait_list = Hash.new
-traits.each do |trait|
+# These two we want to get the full traits with no splitting up
+complex_traits = [traits[0], traits[5]]
+complex_traits.each do |trait|
+  t_ary = foo.card_trait_list(trait, 'ASDFGHJKL')
+  trait_list[trait] = t_ary
+end
+
+simple_traits = traits[1..4]
+simple_traits = simple_traits + traits[6..-1]
+simple_traits.each do |trait|
   t_ary = foo.card_trait_list(trait, '\|?,? ')
   trait_list[trait] = t_ary
 end
@@ -46,8 +56,11 @@ puts "<head>"
 #puts '<link rel="stylesheet" type="text/css" href="/hex/tables.css">'
 puts "</head>"
 puts '<h1>Example Searches</h1>'
-puts '<table style="table-layout:fixed; overflow:hidden; white-space:nowrap;">'
-trait_list.each_pair do |trait, t_ary|
+puts '<table border=0><tr><td>'
+puts '<table border=0 style="table-layout:fixed; overflow:hidden; white-space:nowrap;">'
+traits.each do |trait|
+  t_ary = trait_list[trait]
+#trait_list.each_pair do |trait, t_ary|
   next if trait =~ /ATK|Health/
   count += 1
   if count % @num_cols == 1
@@ -73,34 +86,40 @@ end
 if count % @num_cols == 1
   puts "</tr>"
 end
+puts "</table></td>"
 
 # Special Handling for ATK and Health
-puts "<tr><td valign=top colspan=2>"
+puts '<td valign=top><table border=0 style="table-layout:fixed; overflow:hidden; white-space:nowrap;">'
+puts "<tr><td valign=top colspan=5>"
 puts "<h2>ATK</h2>"
 puts '<ul>'
-atk_ary = trait_list['ATK']
-health_ary = trait_list['Health']
+#atk_ary = trait_list['ATK']
+#health_ary = trait_list['Health']
+atk_ary = foo.troop_atk_values()
+health_ary = foo.troop_health_values()
 atk_ary.each do |a|
   print "  <li> #{search_link('ATK', a, a)}"
-  health_ary.each do |h|
+  h_vals = foo.health_for_troops_with_atk(a)
+  h_vals.each do |h|
     link = search_ah_link(a, h, "#{a}/#{h}")
     print " - #{link}"
   end
   puts
 end
-puts '</ul></td>'
-puts "<td valign=top colspan=2><h2>Health</h2>"
+puts '</ul>'
+puts "<h2>Health</h2>"
 puts '<ul>'
 health_ary.each do |h|
   print "  <li> #{search_link('Health', h, h)}"
-  atk_ary.each do |a|
+  a_vals = foo.atk_for_troops_with_health(h)
+  a_vals.each do |a|
     link = search_ah_link(a, h, "#{a}/#{h}")
     print " - #{link}"
   end
   puts
 end
 puts '</ul></td></tr>'
-puts '</table>'
+puts '</table></td></tr></table>'
 
 #binding.pry
 puts "</body>\n</html>"
